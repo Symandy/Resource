6e50d1b4e4621c9eaba6949c02e4f32c9fc8cfe9
Samaël Villette <villette.samael@gmail.com>
2022-02-14 10:56:28 +0100

    refactor(timestampable): optimized imports (#3)

--- src/Model/TimestampableInterface.php
+++ src/Model/TimestampableInterface.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Symandy\Component\Resource\Model;
 
-use DateTimeInterface;
-
 interface TimestampableInterface extends CreatableInterface, UpdatableInterface
 {
 
